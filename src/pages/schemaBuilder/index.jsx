b00d9c3760eb9eeefd695c83d688333fea2b7196
samiulfahad <samiul_fahad@yahoo.com>
2026-03-02 00:02:20 +0600

    Schema Builder

--- src/pages/schemaBuilder/index.jsx
+++ src/pages/schemaBuilder/index.jsx
@@ -744,7 +744,7 @@ function SectionCard({ section, index, total, fieldErrors }) {
   );
 }
 
-import SchemaRenderer from "./schemaRenderer";
+import SchemaRenderer from "./SchemaRenderer";
 
 function SkeletonLoader() {
   return (
